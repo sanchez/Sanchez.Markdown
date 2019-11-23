77f08ea0f2386e527a37284856de93b67901ddf8
sanchez <dfitz360@gmail.com>
2019-11-24 09:13:02 +1000

    Added processing of italics and bold

--- Sanchez.Markdown.Parser/Parsers/InlineParser.fs
+++ Sanchez.Markdown.Parser/Parsers/InlineParser.fs
@@ -4,6 +4,8 @@ open Sanchez.Markdown.Parser.Models
 
 module InlineParser =
 
+    type InlineParserType = char list -> char list -> Symbols.Inline option list
+
     let private separateHeadOut (characters: List<char>) =
         (characters.Head, characters.Tail)
 
@@ -15,7 +17,51 @@ module InlineParser =
         |> Symbols.PlainText
         |> Some
 
-    let private parsers: List<List<char> -> Symbols.Inline option * char list> = []
+    let rec private ProcessBold (characters: char list) (startChar: char) (containingChars: char list) (processor: InlineParserType) =
+        if characters.Length = 1 then
+            (None, characters)
+        else
+            let currentChar = characters.Head
+            let nextChar = characters.Tail.Head
+            if nextChar = currentChar && currentChar = startChar then
+                processor containingChars []
+                |> List.filter (fun x -> x.IsSome)
+                |> List.map (fun x -> x.Value)
+                |> Symbols.SimpleInlineSymbol
+                |> Symbols.Bold
+                |> (fun x -> (Some x, characters.Tail.Tail))
+            else
+                ProcessBold characters.Tail startChar (containingChars @ [characters.Head]) processor
+
+    let rec private ProcessItalics (characters: char list) (startChar: char) (containingChars: char list) (processor: InlineParserType) =
+        if characters.Length = 0 then
+            (None, characters)
+        else 
+            let currentChar = characters.Head
+            if currentChar = startChar then
+                processor containingChars []
+                |> List.filter (fun x -> x.IsSome)
+                |> List.map (fun x -> x.Value)
+                |> Symbols.SimpleInlineSymbol
+                |> Symbols.Italics
+                |> (fun x -> (Some x, characters.Tail))
+            else
+                ProcessItalics characters.Tail startChar (containingChars @ [characters.Head]) processor
+
+    let private ProcessBoldItalics (characters: char list) (processor: InlineParserType) =
+        let first = characters.Head
+        if first = '*' || first = '_' then
+            let second = characters.Tail.Head
+            if second = first then
+                ProcessBold characters.Tail.Tail first [] processor
+            else
+                ProcessItalics characters.Tail first [] processor
+        else
+            (None, characters)
+
+    let private parsers: List<char list -> InlineParserType -> Symbols.Inline option * char list> = [
+        ProcessBoldItalics
+    ]
 
     let rec processInline (chars: List<char>) (emptyChars: List<char>) =
         if chars.IsEmpty then [CreatePlainText emptyChars]
@@ -23,13 +69,13 @@ module InlineParser =
             let parser =
                 parsers
                 |> List.tryFind (fun x ->
-                    let (s, _) = x chars
+                    let (s, _) = x chars processInline
                     s.IsSome
                     )
 
             match parser with
             | Some (x) ->
-                let (s, remainderLines) = x chars
+                let (s, remainderLines) = x chars processInline
                 if emptyChars.IsEmpty then
                     s::processInline remainderLines []
                 else
