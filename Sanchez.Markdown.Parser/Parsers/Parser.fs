19921ae26752897589a650cacd9745266544355f
Daniel Fitz <dfitz360@gmail.com>
2019-11-25 19:44:05 +1000

    Applied @logan12358 suggested changes

--- Sanchez.Markdown.Parser/Parsers/Parser.fs
+++ Sanchez.Markdown.Parser/Parsers/Parser.fs
@@ -14,23 +14,13 @@ let private inlineParsers: List<char list -> InlineParserType -> Inline option *
 let rec processInline (chars: char list) (emptyChars: char list) (processor: InlineParserType) =
     if chars.IsEmpty then [CreatePlainText emptyChars]
     else
-        let parser =
-            inlineParsers
-            |> List.tryFind (fun x ->
-                let (s, _) = x chars processor
-                s.IsSome
-                )
-
-        match parser with
-        | Some (x) ->
-            let (s, remainderLines) = x chars processor
-            if emptyChars.IsEmpty then
-                s::processInline remainderLines [] processor
-            else
-                let plain = CreatePlainText emptyChars
-                plain::s::processInline remainderLines [] processor
-        | None ->
-            processInline chars.Tail (emptyChars @ [chars.Head]) processor
+        inlineParsers
+        |> List.map (fun x -> x chars processor)
+        |> List.tryFind (fst >> Option.isSome)
+        |> Option.map (fun (s, remainderChars) -> 
+            let plain = CreatePlainText emptyChars
+            plain::s::processInline remainderChars [] processor)
+        |> Option.defaultWith (fun () -> processInline chars.Tail (emptyChars @ [chars.Head]) processor)
 
 let rec ParseInlines (chars: char list) (emptyChars: char list) =
     processInline chars emptyChars ParseInlines
@@ -49,16 +39,8 @@ let private blockParsers: List<string list -> BlockParserType -> InlineParserTyp
 let rec ParseLines (lines: string list) =
     if lines.IsEmpty then []
     else
-        let result =
-            blockParsers
-            |> List.map (fun x -> x lines ParseLines ParseInlines)
-            |> List.tryFind (fun x -> 
-                let (s, _) = x
-                s.IsSome)
-
-        match result with
-        | Some (x) ->
-            let (s, remainderLines) = x
-            s.Value::ParseLines remainderLines
-        | None ->
-            ParseLines lines.Tail
+        blockParsers
+        |> List.map (fun x -> x lines ParseLines ParseInlines)
+        |> List.tryFind (fst >> Option.isSome)
+        |> Option.map (fun (s, remainderLines) -> s.Value::ParseLines remainderLines)
+        |> Option.defaultWith (fun () -> ParseLines lines.Tail)
