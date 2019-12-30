735aa5c6a4b09291c7459ae57c683802a42f0a07
Daniel Fitz <dfitz360@gmail.com>
2019-12-30 15:34:21 +1000

    Added a quick way to fetch information about the document metadata, needs to be improved in the future

--- Sanchez.Markdown.Symbols/Block.fs
+++ Sanchez.Markdown.Symbols/Block.fs
@@ -21,6 +21,8 @@ type Block =
 and DocumentSymbol (metadata: Map<string, string>, content: Block list) =
     member this.Metadata = metadata
     member this.Content = content
+    member this.GetMetaField key =
+        metadata |> Map.tryFind key
 
 and SimpleSymbol (content: Inline list) =
     member this.Content = content
